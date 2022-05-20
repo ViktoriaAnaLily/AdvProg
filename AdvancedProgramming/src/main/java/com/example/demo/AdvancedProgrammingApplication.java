a313b4fb33273afd19d1452338047372165a31f9
Viktoria_Gordeeva_5 <Viktoria_Gordeeva_5@LAPTOP-IRVP97I1>
2022-05-20 16:36:09 +0200

    LOG

---
+++ AdvancedProgramming/src/main/java/com/example/demo/AdvancedProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedProgrammingApplication.class, args);
+	}
+
+}
